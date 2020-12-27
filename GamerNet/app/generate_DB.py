e731169b1fa8671f712ce63e421409620cb806e3
Diogo <diogo.fernandes77@ua.pt>
2020-12-27 18:11:52 +0000

    graph generator + gitignore

--- GamerNet/app/generate_DB.py
+++ GamerNet/app/generate_DB.py
@@ -3,19 +3,49 @@ from rdflib.namespace import DC, FOAF
 import random
 import json
 import os
-from GamerNet.settings import BASE_DIR
+#from GamerNet.settings import BASE_DIR
 
 g = Graph()
 NS = Namespace('http://GamerNetLibrary.com/')
 
 
 def generation():
-    with open(os.path.join(BASE_DIR, 'app/data/games_info.json')) as file_parser:
+    with open('data/games_full_info.json') as file_parser:
         json_games = json.load(file_parser)
+        tmp_array = json_games.keys()
+        count = 0
+        appid_array = [ key for key in tmp_array]
+
 
         for idx in json_games:
-            game = json_games[idx]
-            subject = json_games["appid"]
-            g.add(subject, RDF.type, NS["game"])
-            g.add(subject,NS.name, Literal(game["name"]))
 
+            game = json_games[idx]["data"]
+
+            subject = NS[appid_array[count]]
+
+            g.add((subject, RDF.type, NS[game["type"]]))
+            g.add((subject,NS.name, Literal(game["name"])))
+            g.add((subject, NS.release_date, Literal(game["release_date"]["date"])))
+            g.add((subject, NS.description, Literal(game["short_description"])))
+            g.add((subject, NS.image, Literal(game["header_image"])))
+            g.add((subject, NS.website, Literal(game["website"])))
+
+            count += 1
+
+
+
+
+def graph_export():
+    g.bind("dc", DC)
+    g.bind("foaf", FOAF)
+    g.bind("game", NS)
+
+    of = open("graph.n3", "wb")
+    of.write(g.serialize(format="n3"))
+    of.close()
+
+
+
+
+generation()
+graph_export()
