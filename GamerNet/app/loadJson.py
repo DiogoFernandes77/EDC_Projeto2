5843dbad925afd4fc459baa9a6c77f4b6e1e2441
Pedro <pedroalves99@ua.pt>
2020-12-27 17:30:30 +0000

    + json

--- GamerNet/app/loadJson.py
+++ GamerNet/app/loadJson.py
@@ -8,4 +8,26 @@ def extract_games():
     with open('data/games_info.json') as g:
         games = json.load(g)
 
-        #for game in games:
+        i = 0
+        games_info= {}
+        for game in games:
+            game_info = requests.get(complement_url + '?appids=' + game)
+
+            json_info = json.loads(game_info.text)
+
+            games_info.update(json_info)
+            #print("game --- " + str(game_info.text))
+            #print(type(game_info.text))
+            i+=1
+
+            if i > 100:
+                print("Got 100 games")
+                with open('data/games_full_info.json', 'w') as w:
+                    w.write(json.dumps(games_info, indent=4))
+                break
+
+            print(i)
+
+
+
+extract_games()
