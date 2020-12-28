36a061e7df7d931c488ef21b487f70445696bcb5
Diogo <diogo.fernandes77@ua.pt>
2020-12-28 20:55:22 +0000

    geraçao de grafos completa

--- GamerNet/app/generate_DB.py
+++ GamerNet/app/generate_DB.py
@@ -3,19 +3,27 @@ from rdflib.namespace import DC, FOAF
 import random
 import json
 import os
-#from GamerNet.settings import BASE_DIR
+
+# from GamerNet.settings import BASE_DIR
 
 g = Graph()
 NS = Namespace('http://GamerNetLibrary.com/')
+names = ["Pedro", "Diogo", "Daniel", "Francisco", "Mario", "Maria", "Joana", "Gustavo", "Rodrigo", "Santiago",
+         "Henrique", "Antonio", "Jorge", "Eduardo", "Eduarda", "Milene", "Sara", "Marta", "Antonieta", "Mariana",
+         "Ricardo", "Silvia"]
+surnames = ["Fernandes", "Alves", "Pereira", "Pomar", "Martins", "Pinheiro", "Pinho", "Nunes", "Machado", "Esteves",
+            "Freitas"]
 
+
+person_number = 20
+tmp = ""
 
 def generation():
     with open('data/games_full_info.json') as file_parser:
         json_games = json.load(file_parser)
         tmp_array = json_games.keys()
         count = 0
-        appid_array = [ key for key in tmp_array]
-
+        appid_array = [key for key in tmp_array]
 
         for idx in json_games:
 
@@ -24,7 +32,8 @@ def generation():
             subject = NS[appid_array[count]]
 
             g.add((subject, RDF.type, NS[game["type"]]))
-            g.add((subject,NS.name, Literal(game["name"])))
+
+            g.add((subject, NS.name, Literal(game["name"])))
             g.add((subject, NS.release_date, Literal(game["release_date"]["date"])))
             g.add((subject, NS.description, Literal(game["short_description"])))
             g.add((subject, NS.image, Literal(game["header_image"])))
@@ -40,13 +49,51 @@ def generation():
             if "genres" in game.keys():
                 for genre in game["genres"]:
                     g.add((subject, NS.genre, Literal(genre["description"])))
-            if "dlc" in game.keys():
-                for dlc in game["dlc"]:
-                    g.add((subject, NS.dlc, NS["dlc" + str(dlc)]))
             if "price_overview" in game.keys():
                 g.add((subject, NS.price, Literal(game["price_overview"]["final_formatted"])))
 
             count += 1
+
+
+
+def people_generation():
+    for id in range(0, person_number):
+        name = random.choice(names)
+        surname = random.choice(surnames)
+        username = name + "_" + surname + str(id)
+        subject = NS["Account" + str(id)]
+
+        g.add((subject, RDF.type, FOAF.Person))
+        g.add((subject,FOAF.nick,Literal(username)))
+        g.add((subject,FOAF.name,Literal(name + " " + surname)))
+
+        #games owned
+        n_games_owned = random.randint(1, 10)
+        games_list = list(g.subjects(RDF.type, NS.game))
+        random.shuffle(games_list)
+        for i in range(0, n_games_owned):
+            g.add((subject,NS.owns, games_list[i]))
+
+
+
+def people_friends_generation():
+    people = list(g.subjects(RDF.type, FOAF.Person))
+    random.shuffle(people)
+    pl_iter = people.copy()
+
+    for i in range(0,len(people)):
+        friends_list = []
+        relation_number = random.randint(0, 10)
+        subject = people.pop()
+        for x in range(0, relation_number):
+            friend = None
+            while(friend == subject) or (friend in friends_list) or (friend == None):
+                friend = random.choice(pl_iter)
+            friends_list.append(friend)
+            g.add((subject, FOAF.knows, friend))
+
+
+
 
 
 
@@ -61,7 +108,7 @@ def graph_export():
     of.close()
 
 
-
-
 generation()
+people_generation()
+people_friends_generation()
 graph_export()
