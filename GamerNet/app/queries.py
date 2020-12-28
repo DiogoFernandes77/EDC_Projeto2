8498b7ca8b8548a0c30da387f884e5691e5e5f72
Pedro <pedroalves99@ua.pt>
2020-12-28 13:35:26 +0000

    game store a funcionar, da p pesquisar por genero de jogo

--- GamerNet/app/queries.py
+++ GamerNet/app/queries.py
@@ -2,6 +2,47 @@ from s4api.graphdb_api import GraphDBApi
 from s4api.swagger import ApiClient
 import json
 
+def get_genres(): #devolve uma lista com todos o géneros de jogos, sem repetidos por ordem alfabética
+    query = """select  ?genre
+    where
+    {
+        ?name a game:game .
+        ?name game:genre ?genre.
+
+    }group by ?genre
+    order by asc(?genre)"""
+
+    result = queryDB(query)
+    return [x["genre"]["value"] for x in result]
+
+def get_games():
+    query = """select  ?name ?gameName  ?image ?description
+        where 
+        {
+            ?name a game:game .
+            ?name game:description ?description. 
+            ?name game:name ?gameName.
+            ?name game:image ?image.
+        }"""
+
+    result = queryDB(query)
+    return [(x["image"]["value"], x["gameName"]["value"], x["description"]["value"], x["name"]["value"]) for x in result]
+
+def get_games_by_genre(genre):
+    query = """select  ?name ?gameName  ?image ?description
+            where 
+            {
+                ?name a game:game .
+                ?name game:description ?description. 
+                ?name game:name ?gameName.
+                ?name game:image ?image.
+                ?name game:genre \'""" + genre + """\'.
+            }"""
+
+    result = queryDB(query)
+    return [(x["image"]["value"], x["gameName"]["value"], x["description"]["value"], x["name"]["value"]) for x in
+            result]
+
 def queryDB(query):
     query = """
         PREFIX game: <http://GamerNetLibrary.com/>
@@ -16,17 +57,22 @@ def queryDB(query):
     else:
         payload = {"query": query}
         result = accessor.sparql_select(body=payload, repo_name=repo)
-        return json.loads(result)
+        result_json = json.loads(result)
+        return result_json["results"]["bindings"]
 
 
-result_json = queryDB("""select  ?name ?gameName  ?image ?description
-where 
-{
-    ?name a game:game .
-    ?name game:description ?description. 
-    ?name game:name ?gameName.
-    ?name game:image ?image.
-}""")
+def test():
+    result_json = queryDB("""select  ?name ?gameName  ?image ?description
+    where 
+    {
+        ?name a game:game .
+        ?name game:description ?description. 
+        ?name game:name ?gameName.
+        ?name game:image ?image.
+    }""")
 
-print(result_json["results"]["bindings"][1]["description"]["value"])
-print([x["image"]["value"] for x in result_json["results"]["bindings"]])
+    print(result_json["results"]["bindings"][1]["description"]["value"])
+    print([x["image"]["value"] for x in result_json["results"]["bindings"]])
+
+#print(get_genres())
+#print(get_games_by_genre('Free to Play'))
--- GamerNet/app/views.py
+++ GamerNet/app/views.py
@@ -4,7 +4,10 @@ from datetime import date
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+#from queries import *
 from django.urls import reverse
+
+from app.queries import *
 
 global prev_id
 prev_id = 0
@@ -56,8 +59,39 @@ def admin_update(request, countryID): #updating  view
 
     return render(request, 'admin.html', tparams)
 
-def news(request):
-    return render(request, 'news.html')
+global prev_gen
+prev_gen = "Select game gender"
+
+def store(request):
+    global prev_gen
+    genres_list = get_genres()
+    game_list = get_games()
+
+    print(game_list)
+    #print(genres_list)
+
+
+    if request.method == "GET":  # carregar a pag
+        sel_gen = "Select game gender"
+
+    if request.method == "POST" and "genderSelect" in request.POST:
+        sel_gen = request.POST["genderSelect"]
+        print(sel_gen)
+
+    if prev_gen == sel_gen:
+        sel_gen = "Select game gender"
+    else:
+        prev_gen = sel_gen
+
+    if sel_gen != "Select game gender":
+        game_list = get_games_by_genre(str(sel_gen))
+
+    tparams = {
+        'default_message': sel_gen,
+        'genres': genres_list,
+        'game_properties': game_list
+    }
+    return render(request, 'news.html', tparams)
 
 
 
